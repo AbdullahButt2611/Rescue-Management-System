d2ed063f08f107fecd290fa23b80782a864f33ed
AbdullahYaqub72 <abdullahyaqub555@gmail.com>
2021-12-08 21:22:15 +0500

    Created the backend classes
    These classes will help in achieving our goal to maintain the core functionality of the project

---
+++ BackEnd Classes.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class Admin:
+    UserName=""
+    Password=""
+
+
+# In[1]:
+
+
+class Hospital:
+    Name=""
+    Location=""
+    Land_Line=""
+    def Add_Hospital():
+    def Display_Hospital():
+    
+
+
+# In[2]:
+
+
+class Worker:
+    Name=""
+    Age=0
+    CNIC=""
+    ID=""
+    Shift=""
+    Status=""
+    Gender=""
+    Salary=0
+    Contact=""
+    Opertaions=0
+    def Add_Worker():
+    def Update_Worker():
+    def Search_Worker():
+    def Delete_Worker():
+    
+
+
+# In[3]:
+
+
+class Location:
+    Name=""
+    Distance=0.0
+    Area_Code=""
+    time=0
+    Incidents-=0
+    def Add_Location():
+    def Update_Location():
+    def Search_Location():
+    def Delete_Location():
+    
+
+
+# In[ ]:
+
+
+class Patient:
+    Name=""
+    Age=0
+    Condition=""
+    Id=0
+    Location=""
+    History=""
+    def Add_Request():
+    def Accept_Request():
+    def Issue_Death_Certificate():
+
+
+# In[ ]:
+
+
+class Ambulance:
+    Avaialable=False
+    Registration_Number=""
+    No_Oxygen_Pumps=0
+    Driver_Name=""
+    ID=0
+    def Add_Ambulance():
+    def Update_Ambulance():
+    def Search_Ambulance():
+    def Delete_Ambulance():
+    def Maintain_Ambulance():
+    def Park_Ambulance():
+        
+
+
+# In[ ]:
+
+
+class Funds:
+    Funds=""
+    Name=""
+    CNIC=""
+    ID=0
+    City=""
+    Account_No=""
+    Contact_No=""
+    def Add_Funds():
+    def Display_Funds():
+        
+
