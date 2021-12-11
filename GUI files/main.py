c2124cb136cca21534a2bf03eb1814c842409fd8
AbdullahButt2611 <abutt2210@gmail.com>
2021-12-11 18:06:56 +0500

    Back-End-Develop_LoadScreen
    I have used splash screen for making the backend of the load screen and then performing all other tasks simultaneously

--- GUI files/main.py
+++ GUI files/main.py
@@ -1,10 +1,58 @@
 import sys
 import platform
-from pySide2 import QtCore, QtGui, QtWidgets
-from pySide2.QtWidgets import *
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
+from PyQt5.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from loadingscreen import Ui_SplashScreen
+from SignInScreen import Ui_MainWindow
 
+counter = 0
+
+class SignIn(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
 class SplashScreen(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
-        # self.ui =
+        self.ui = Ui_SplashScreen()
+        self.ui.setupUi(self)
+
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)   #Remove title bar
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+        self.shadow = QGraphicsDropShadowEffect(self)   #shadow Effect
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor(0, 0, 0, 60))
+        self.ui.dropShadowFrame.setGraphicsEffect(self.shadow)
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.progress)
+
+        self.timer.start(35)
+
+        self.show()
+
+    def progress(self):
+
+        global counter
+        self.ui.progressBar.setValue(counter)
+        if counter>100:
+            self.timer.stop()
+
+            self.main = SignIn()
+            self.main.show()
+
+            self.close()
+        counter += 1
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = SplashScreen()
+    sys.exit(app.exec_())
